c58762e3647bef6ef2d13f457e783ae498f819d0
Stephen von Takach <steve@acaprojects.com>
2019-06-12 14:08:14 +1000

    (file_handler) deprecate usage

--- src/action-controller/file_handler.cr
+++ src/action-controller/file_handler.cr
@@ -1,30 +1,11 @@
-class ActionController::FileHandler < ::HTTP::StaticFileHandler
-  MIME_TYPES = {
-    ".txt"      => "text/plain",
-    ".htm"      => "text/html",
-    ".html"     => "text/html",
-    ".css"      => "text/css",
-    ".js"       => "application/javascript",
-    ".png"      => "image/png",
-    ".gif"      => "image/gif",
-    ".jfif"     => "image/jpeg",
-    ".jpe"      => "image/jpeg",
-    ".jpeg"     => "image/jpeg",
-    ".jpg"      => "image/jpeg",
-    ".webp"     => "image/webp",
-    ".appcache" => "text/cache-manifest",
-    ".ico"      => "image/x-icon",
-    ".json"     => "application/json",
-    ".svg"      => "image/svg+xml",
-    ".woff"     => "font/woff",
-    ".woff2"    => "font/woff2",
-    ".ttf"      => "font/ttf",
-    ".otf"      => "font/opentype",
-  } of String => String
+require "mime"
 
-  # Allow additional mime types to be configured
-  private def mime_type(path)
-    mime = MIME_TYPES[File.extname(path)]?
-    mime || "application/octet-stream"
+# This is deprecated in favour of the built in MIME store and handlers
+# this is updated to provide compatibility but will be removed in the future
+class ActionController::FileHandler < ::HTTP::StaticFileHandler
+  class MIME_TYPES
+    def self.[]=(key, value)
+      ::MIME.register(key, value)
+    end
   end
 end
