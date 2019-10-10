0584c3ff51c1f94f8ff5f4c746123f7443e0cc78
Stephen von Takach <steve@acaprojects.com>
2019-10-10 14:57:06 +1100

    feat(log handler): add support for filtering sensitive keys

--- src/action-controller/log_handler.cr
+++ src/action-controller/log_handler.cr
@@ -4,16 +4,19 @@ class ActionController::LogHandler
   include HTTP::Handler
 
   # Initializes this handler to log to the given `IO`.
-  def initialize(@io : IO, &@tags : Proc(HTTP::Server::Context, String))
+  def initialize(@io : IO, filter = nil, &@tags : Proc(HTTP::Server::Context, String))
     @logger = Channel(String).new(128)
+    @filter = filter ? filter.to_a.map(&.to_s) : [] of String
     spawn { write_logs! }
   end
+
+  @filter : Array(String)
 
   def call(context)
     elapsed = Time.measure { call_next(context) }
     elapsed_text = elapsed_text(elapsed)
     tags = @tags.call(context)
-    @logger.send("method=#{context.request.method} status=#{context.response.status_code} path=#{context.request.resource} duration=#{elapsed_text}#{tags}")
+    @logger.send("method=#{context.request.method} status=#{context.response.status_code} path=#{filter_path context.request.resource} duration=#{elapsed_text}#{tags}")
   rescue e
     tags = begin
       @tags.call(context)
@@ -21,7 +24,7 @@ class ActionController::LogHandler
       @logger.send "error building custom tag list\n#{e.inspect_with_backtrace}"
       nil
     end
-    @logger.send "method=#{context.request.method} status=500 path=#{context.request.resource}#{tags}\n#{e.inspect_with_backtrace}"
+    @logger.send "method=#{context.request.method} status=500 path=#{filter_path context.request.resource}#{tags}\n#{e.inspect_with_backtrace}"
     raise e
   end
 
@@ -36,6 +39,20 @@ class ActionController::LogHandler
     return "#{millis.round(2)}ms" if millis >= 1
 
     "#{(millis * 1000).round(2)}µs"
+  end
+
+  private def filter_path(path)
+    return path if @filter.empty?
+    path.gsub(/(\?|\&)([^&;=]+)=([^&;=]+)/) do
+      filter = false
+      @filter.each do |key|
+        if $2 == key
+          filter = true
+          break
+        end
+      end
+      filter ? "#{$1}#{$2}=[FILTERED]" : "#{$1}#{$2}=#{$3}"
+    end
   end
 
   private def write_logs!
