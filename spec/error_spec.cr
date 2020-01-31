d0d05315c62923f4f5abd455da4b988d68e7f3de
Caspian Baska <caspianbaska@gmail.com>
2020-01-31 14:40:23 +1100

    test(error): factor ErrorHandler specs into specific cases

--- spec/error_spec.cr
+++ spec/error_spec.cr
@@ -1,8 +1,11 @@
 require "./spec_helper"
 
 describe ActionController::ErrorHandler do
-  it "should initialize an error handler" do
-    prod = ActionController::ErrorHandler.new(false, ["X-Request-ID"])
-    dev = ActionController::ErrorHandler.new(true, ["X-Request-ID"])
+  it "should initialize a production error handler" do
+    ActionController::ErrorHandler.new(development: false, persist_headers: ["X-Request-ID"])
+  end
+
+  it "should initiaze a development error handler" do
+    ActionController::ErrorHandler.new(development: true, persist_headers: ["X-Request-ID"])
   end
 end
