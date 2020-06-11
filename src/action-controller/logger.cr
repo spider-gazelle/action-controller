5118f9c8bb1606b628d481fdfb97319dae733d6f
Caspian Baska <caspianbaska@gmail.com>
2020-06-11 22:58:45 +1000

    feat: support new `data` in Log::Entry

--- src/action-controller/logger.cr
+++ src/action-controller/logger.cr
@@ -8,15 +8,17 @@ module ActionController
       label = entry.severity.label
       timestamp = entry.timestamp
       context = entry.context
+      data = entry.data
       io << String.build do |str|
         str << "level=" << label << " time="
         timestamp.to_rfc3339(str)
         str << " source=" << entry.source
 
         # Add context tags
-        context.as_h?.try &.each do |k, v|
+        {context, data}.each &.each do |k, v|
           str << " " << k << "=" << v
         end
+
         str << " message=" << entry.message unless entry.message.empty?
 
         if exception = entry.exception
