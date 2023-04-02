36bae1dac7cb91c5bd8d3c276ba12f948e1b4887
Stephen von Takach <steve@place.technology>
2023-04-03 09:20:05 +1000

    feat: add helpers for rendering errors

--- src/action-controller/errors.cr
+++ src/action-controller/errors.cr
@@ -1,4 +1,57 @@
+require "json"
+require "yaml"
+
 class ActionController::Error < Exception
+  class Unauthorized < ActionController::Error
+  end
+
+  class Forbidden < ActionController::Error
+  end
+
+  class NotFound < ActionController::Error
+  end
+
+  class Conflict < ActionController::Error
+  end
+
+  # provides a response object for rendering errors
+  struct CommonResponse
+    include JSON::Serializable
+    include YAML::Serializable
+
+    getter error : String?
+    getter backtrace : Array(String)?
+
+    def initialize(error, backtrace = true)
+      @error = error.message
+      @backtrace = backtrace ? error.backtrace : nil
+    end
+  end
+
+  # Provides details on available data formats
+  struct ContentResponse
+    include JSON::Serializable
+    include YAML::Serializable
+
+    getter error : String
+    getter accepts : Array(String)? = nil
+
+    def initialize(@error, @accepts = nil)
+    end
+  end
+
+  # Provides details on which parameter is missing or invalid
+  struct ParameterResponse
+    include JSON::Serializable
+    include YAML::Serializable
+
+    getter error : String
+    getter parameter : String? = nil
+    getter restriction : String? = nil
+
+    def initialize(@error, @parameter = nil, @restriction = nil)
+    end
+  end
 end
 
 class ActionController::CookieSizeExceeded < ActionController::Error
